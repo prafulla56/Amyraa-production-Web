ac02a721fa63ab648ff734cc7dae35d812eb1fd7
Prafulla Mishra <126856146+prafulla56@users.noreply.github.com>
2025-08-13 20:18:40 +0530

    Improve performance of data processing module Optimize the data processing module to enhance performance and reduce latency during execution. This includes refactoring code and implementing more efficient algorithms.

--- src/test/java/PM/Base/BaseTest.java
+++ src/test/java/PM/Base/BaseTest.java
@@ -21,8 +21,8 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.get("https://amyraa.app/"); 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.findElement(By.xpath("//a[text()='Login / Register']")).click();
-        //driver.findElement(By.xpath("//h4[text()='Register']")).click();//When need to test Registeration of User
+        driver.findElement(By.xpath("//span[text()='Login/Register']")).click();
+        driver.findElement(By.xpath("//h4[text()='Register']")).click();//When need to test Registeration of User
     }
     @AfterMethod
     public void stop() throws InterruptedException {
